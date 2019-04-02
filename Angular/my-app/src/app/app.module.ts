65d8214392238c180617b85ec9cb040aa251c049
jorgeandres6 <jorgeandres6@msn.com>
2019-04-02 17:45:46 -0500

    Listo esqueleto login Angular

---
+++ Angular/my-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
